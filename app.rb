a0f0664065c9a3f1f599306ec04273a05a9e162c
Christian Shaffer <calexandershaffer@gmail.com>
2023-10-18 22:16:39 -0400

    Add functionality for multiple words
    Had to refactor the code. Found some clever ways to minimize it. Now
    using an outer and inner loop to create every possible instance of a
    word before it is checked in the dictionary. All done.

--- app.rb
+++ app.rb
@@ -1,39 +1,21 @@
-require 'pry-byebug'
-
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
-  #word_container = string.split(" ")
-  appeared = []
+  word_container = string.downcase.gsub(/[^a-z\s]/, '').split(" ")
   result = {}
-  string_to_check = ''
-  string.each_char do |letter|
-    string_to_check += letter
-    if dictionary.include?(string_to_check)
-      appeared.push(string_to_check)
+
+  word_container.each do |word|
+    (0...word.length).each do |start_index|
+      (start_index...word.length).each do |end_index|
+        substring = word[start_index..end_index]
+        if dictionary.include?(substring)
+          result[substring] = result[substring].to_i + 1
+        end
+      end
     end
   end
-  string_to_check.each_char do |letter|
-    string_to_check = string_to_check[1..-1]
-    if dictionary.include?(string_to_check)
-      appeared.push(string_to_check)
-    end
-  end
-  appeared.each_with_index do |word, index|
-    count = 0
-    if word == appeared[index]
-      count += 1
-      result[word] = count
-    end
-  end
+
   result
 end
 
-p substrings('below low', dictionary)
-
-# Does the count work with multiple words?
-
-# string = 'hey there'
-# word_container = string.split(//)
-# p string
-# p word_container
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
